f55c708af7e107e009310637de0ef5d13b203798
Keks <keks@htmlacademy.ru>
2022-11-06 11:44:22 +0300

    :heavy_check_mark: Сборка #1

---
+++ 1/js/main.js
@@ -0,0 +1,5 @@
+import './util.js';
+import {createPhotoDescription} from './data.js';
+import './miniatures.js';
+
+createPhotoDescription();
---
+++ 1/js/miniatures.js
@@ -0,0 +1,21 @@
+import {createPhotoDescription} from './data.js';
+
+const pictureTemplate = document.querySelector('#picture').content;
+const pictures = pictureTemplate.querySelector('.picture');
+
+const usersPictures = document.querySelector('.pictures');
+const createFragment = document.createDocumentFragment();
+
+const createPhotoAll = createPhotoDescription();
+
+createPhotoAll.forEach(({url, likes, comments}) => {
+  const picture = pictures.cloneNode(true);
+  picture.querySelector('.picture__img').src = url;
+  picture.querySelector('.picture__likes').textContent = String(likes);
+  picture.querySelector('.picture__comments').textContent = String(comments);
+
+  createFragment.append(picture);
+  return picture;
+});
+
+usersPictures.append(createFragment);
