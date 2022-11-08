9c70a086f52ea52aa2d61aada9baf69ad09afdbd
Liza <richardaddy333@gmail.com>
2022-11-08 15:22:18 +0300

    Добавляет валидацию полю комментария;

--- js/main.js
+++ js/main.js
@@ -1,4 +1,2 @@
-import './util.js';
-import './data.js';
 import './miniatures.js';
 import './user-modal.js';
--- js/user-modal.js
+++ js/user-modal.js
@@ -1,9 +1,9 @@
 import {isEscapeKey} from './util.js';
+import {imgUploadForm, pristine} from './user-form.js';
 
 const uploadFile = document.querySelector('#upload-file'); // Поле для загрузки изображения
 const uploadCancel = document.querySelector('#upload-cancel'); // Кнопка для закрытия формы редактирования изображения
 const imgUploadOverlay = document.querySelector('.img-upload__overlay'); // Форма редактирования изображения
-const imgUpload = document.querySelector('.img-upload__form'); // Поле для загрузки нового изображения на сайт
 
 //Удаление обработчика закрытия окна по нажатию на Esc
 const onPopupEscKeydown = (evt) => {
@@ -23,6 +23,7 @@ const closeUserModal = () => {
   imgUploadOverlay.classList.add('hidden');
   document.body.classList.remove('modal-open');
   document.removeEventListener('keydown', onPopupEscKeydown);
+  imgUploadForm.value = '';
 };
 
 uploadFile.addEventListener('change', () => {
@@ -31,4 +32,10 @@ uploadFile.addEventListener('change', () => {
 
 uploadCancel.addEventListener('click', () => {
   closeUserModal();
+});
+
+imgUploadForm.addEventListener('submit', (evt) => {
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
 });
